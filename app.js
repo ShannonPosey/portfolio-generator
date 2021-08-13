e56f47805971b596653b9a2f933e30e648f26a5d
shannon_p <snp1284kasey@aol.com>
2021-08-12 20:10:11 -0400

    created hmtl page using js

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
+const generatePage = require("./src/page-template");
+
+const profileDataArgs = process.argv.slice(2);
 console.log(profileDataArgs);
 
+// Destructuring Assignment example
+const [name, github] = profileDataArgs;
+console.log(name, github);
+
+/* Template literals example
+const generatePage = (userName, githubName) => `Name: ${userName}, GitHub: ${githubName}`;*/
+
+/* Multi-line Template Literal Strings example
+const generatePage = (userName, githubName) => {
+    return `
+    Name: ${userName}
+    GitHub: ${githubName}
+    `;
+};*/
+
+fs.writeFile("./index.html", generatePage(name, github), err => {
+  // this will display an error message if an error occurs
+    if (err) throw new Error (err);
+    console.log("Portfolio complete! Check out index.html to see the output!");
+});
 
 
 
+// const printProfileData = profileDataArr => {
+//     // This
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
 
-const printProfileData = profileDataArr => {
-    // This
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+//     console.log("================");
+//     // Is the same as this ...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
-    console.log("================");
-    // Is the same as this ...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
 
 
 
